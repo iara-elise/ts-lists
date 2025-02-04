d698f36c87018547e063794c46aaa15dcacd5d42
iara-elise <iara.elise.0006@gmail.com>
2025-02-04 13:43:38 -0800

    Removendo arquivos .js.

--- L1-Input-Output/Q09.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var readline = require("readline-sync");
-
-var altura = parseFloat(readline.question("Altura: "));
-var peso = parseFloat(readline.question("Peso: "));
-
-var imc = peso / Math.pow(altura, 2);
-console.log("IMC: " + imc.toFixed(1));
